cc403eca242b47ec5aa4a4635a945253c26b992f
Sihnor <m.rudolph.programming@outlook.com>
2023-09-23 12:43:18 +0200

    Init Commit

---
+++ Source/Project_Nexus/Project_NexusGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Project_NexusGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_NEXUS_API AProject_NexusGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
