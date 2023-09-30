0fb81e06c1575ad247802ea1f7876bc5b5be6109
Sihnor <m.rudolph.programming@outlook.com>
2023-09-30 17:57:27 +0200

    Bearbeitung vom GameMode, GameState, ActualPLayer, PlayerInformation
    ICh habe die bearbeitung der Zeit wieder im GameMode implementeiert und
    nur den Wert im State dekrementiert.
    Ich habe ein Event hinzugefuegt beim erleiden vom Schaden. Dieser wurde
    vom GameMode abboniert und soll ueberprufen ob der getroffene Spieler
    tot ist. Eine StartMatch Methode habe ich hinzugefuegt um die Start
    parameter des States zu setzten.

---
+++ Source/Project_Nexus/Private/GameState/Nexus_GameState.cpp
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameState/Nexus_GameState.h"
+#include "UENUMS/ActualPlayer.h"
+
+ANexus_GameState::ANexus_GameState() {
+	this->RemainingGameTime = 0;
+}
+
+void ANexus_GameState::BeginPlay() {
+	Super::BeginPlay();
+}
+
+int32 ANexus_GameState::GetRemainingTime() const {
+	return this->RemainingGameTime;
+}
+
+void ANexus_GameState::SetRemainingTime(const int32 Duration) {
+	this->RemainingGameTime = Duration;
+}
+
+void ANexus_GameState::DecrementRemainingTime() {
+	this->RemainingGameTime--;
+}
+
+void ANexus_GameState::RefreshHealth(EPlayerEnum HitEnemy) {
+
+	
+	this->ReductionOfLifeEvent.Broadcast(HitEnemy);
+}
---
+++ Source/Project_Nexus/Public/GameModes/Nexus_GameMode.h
@@ -0,0 +1,40 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "Nexus_GameMode.generated.h"
+
+/**
+ * 
+ */
+enum class EPlayerEnum : uint8;
+
+UCLASS()
+class PROJECT_NEXUS_API ANexus_GameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+	ANexus_GameMode();
+
+	virtual void StartMatch() override;
+	void CountDown();
+
+	//class ANexus_GameState* GameState;
+
+	TObjectPtr<class ANexus_GameState> GameState;
+	
+	void CheckIfDeath(EPlayerEnum Player);
+	
+private:
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, meta=(AllowPrivateAccess = "true"))
+	int32 GameDurationInSeconds;
+
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, meta=(AllowPrivateAccess = "true"))
+	int32 WinCondition;
+
+	UPROPERTY()
+	FTimerHandle TH_CountDown;
+
+};
