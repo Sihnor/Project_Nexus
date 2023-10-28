a12896fa4299758b04239fc6f3d80f5e7404d563
Marcel <54372177+Sihnor@users.noreply.github.com>
2023-10-28 14:47:43 +0200

    Add the SoundOPtionsMenu

---
+++ Source/Project_Nexus/Public/UI/Menus/Nexus_SoundOptions.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Base/Nexus_CommonActivatableWidget.h"
+#include "Nexus_SoundOptions.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_NEXUS_API UNexus_SoundOptions : public UNexus_CommonActivatableWidget
+{
+	GENERATED_BODY()
+};
