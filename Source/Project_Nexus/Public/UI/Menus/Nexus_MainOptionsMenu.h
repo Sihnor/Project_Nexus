13ee8344c1da5c6f4af034e966d903be625476d0
Marcel <54372177+Sihnor@users.noreply.github.com>
2023-10-28 14:47:24 +0200

    Filled the OptionsMenu

--- Source/Project_Nexus/Public/UI/Menus/Nexus_MainOptionsMenu.h
+++ Source/Project_Nexus/Public/UI/Menus/Nexus_MainOptionsMenu.h
@@ -13,4 +13,32 @@ UCLASS()
 class PROJECT_NEXUS_API UNexus_MainOptionsMenu : public UNexus_CommonActivatableWidget
 {
 	GENERATED_BODY()
+
+	// Online Multiplayer
+	UPROPERTY(meta = (BindWidget))
+	class UNexus_CommonButtonBase* GameOptionsButton;
+
+	// Online Multiplayer
+	UPROPERTY(meta = (BindWidget))
+	class UNexus_CommonButtonBase* SoundOptionsButton;
+
+	// Online Multiplayer
+	UPROPERTY(meta = (BindWidget))
+	class UNexus_CommonButtonBase* ControllerSetupButton;
+
+	// Online Multiplayer
+	UPROPERTY(meta = (BindWidget))
+	class UNexus_CommonButtonBase* DisplaySettingsButton;
+
+	// Online Multiplayer
+	UPROPERTY(meta = (BindWidget))
+	class UNexus_CommonButtonBase* GraphicsSettingButton;
+
+	// Online Multiplayer
+	UPROPERTY(meta = (BindWidget))
+	class UNexus_CommonButtonBase* CopyrightButton;
+
+	// Online Multiplayer
+	UPROPERTY(meta = (BindWidget))
+	class UNexus_CommonButtonBase* CreditsButton;
 };
