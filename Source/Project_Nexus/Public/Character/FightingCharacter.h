57c4911acc7b07d46814b877f5136d9ba60bc46d
Izmir Rexhepi <izmir.rexhepi27@hotmail.com>
2024-02-12 13:28:40 +0100

    2nd of hating git

--- Source/Project_Nexus/Public/Character/FightingCharacter.h
+++ Source/Project_Nexus/Public/Character/FightingCharacter.h
@@ -26,6 +26,7 @@ enum class ECharacterState : uint8
 	FC_Jumping UMETA(DisplayName = "Jumping"),
 	FC_LightStunned UMETA(DisplayName = "Light_Stunned"),
 	FC_HeavyStunned UMETA(DisplayName = "Heavy_Stunned"),
+	FC_ThrowStunned UMETA(DisplayName = "Throw_Stunned"),
 	FC_BlockStunned UMETA(DisplayName = "Block_Stunned"),
 	FC_BlockCrouchStunned UMETA(DisplayName = "Block_Crouch_Stunned"),
 	FC_Crouching UMETA(DisplayName = "Crouching"),
